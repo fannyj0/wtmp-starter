1744fac58dda972edd2ef3eb7c584fd83821306c
fannyj0 <fanny.jokinen@metropolia.fi>
2020-01-21 13:43:33 +0200

    toka teht

--- src/index.js
+++ src/index.js
@@ -1,1 +1,131 @@
-console.log('Hello console!');
+const highnum = 20;
+const lownum = 1;
+
+let randomNumber = Math.floor(Math.random() * highnum) + lownum;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+let checkGuess = () => {
+  let userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Previous guesses: ';
+  }
+  guesses.textContent += userGuess + ' ';
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = 'Congratulations! You got it right!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = '!!!GAME OVER!!!';
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'red';
+    if(userGuess < randomNumber) {
+      lowOrHi.textContent = 'Last guess was too low!';
+    } else if(userGuess > randomNumber) {
+      lowOrHi.textContent = 'Last guess was too high!';
+    }
+  }
+
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+};
+
+
+guessSubmit.addEventListener('click', checkGuess);
+
+let setGameOver = () => {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener('click', resetGame, resetTimeCounter);
+};
+
+let resetGame = () => {
+  guessCount = 1;
+
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for (let i = 0 ; i < resetParas.length ; i++) {
+    resetParas[i].textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+
+  lastResult.style.backgroundColor = 'white';
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+};
+
+
+
+/*
+const timestamp = Date.now() * 1000;
+ guessSubmit.addEventListener('click', timestamp);
+
+document.write("Your time is: " + timestamp);
+*/
+
+let startTime = Math.floor(Date.now() / 1000); //Get the starting time (right now) in seconds
+localStorage.setItem("startTime", startTime); // Store it if I want to restart the timer on the next page
+
+let startTimeCounter = () => {
+    let now = Math.floor(Date.now() / 1000); // get the time now
+    let diff = now - startTime; // diff in seconds between now and start
+    let m = Math.floor(diff / 60); // get minutes value (quotient of diff)
+    let s = Math.floor(diff % 60); // get seconds value (remainder of diff)
+    m = checkTime(m); // add a leading zero if it's single digit
+    s = checkTime(s); // add a leading zero if it's single digit
+    document.getElementById("timer").innerHTML = m + ":" + s; // update the element where the timer will appear
+    let t = setTimeout(startTimeCounter, 500); // set a timeout to update the timer
+};
+
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    };  // add zero in front of numbers < 10
+    return i;
+}
+
+let newTime = Math.floor(Date.now() / 1000); //Get the starting time (right now) in seconds
+let resetTimeCounter = () =>{
+  let now = Math.floor(Date.now() / 1000); // get the time now
+    let diff = now - newTime; // diff in seconds between now and start
+    let m = Math.floor(diff / 60); // get minutes value (quotient of diff)
+    let s = Math.floor(diff % 60); // get seconds value (remainder of diff)
+    m = checkTime(m); // add a leading zero if it's single digit
+    s = checkTime(s); // add a leading zero if it's single digit
+    document.getElementById("timer").innerHTML = m + ":" + s; // update the element where the timer will appear
+    let t = setTimeout(startTimeCounter, 500); // set a timeout to update the timer
+};
+
+guessSubmit.addEventListener('click', startTimeCounter());
+
+function stopTimeCounter(){
+  if(setGameOver){
+    startTimeCounter.disabled;
+  }
+}
+
+
+
+
+
