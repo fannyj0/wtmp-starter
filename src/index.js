d59ad228c44d9275c253c050ad4813e6ef6b3b2f
fannyj0 <fanny.jokinen@metropolia.fi>
2020-01-28 18:35:24 +0200

    kokeiluja

--- src/index.js
+++ src/index.js
@@ -42,9 +42,43 @@ console.log('Sorted meals', sortedMeals);
 const filrered = meals.filter(price => price < 5); //ei toimi
 console.log('Filtered meals', filrered);
 
+/*
+const filter = (array, test) =>{
+  let passed = [];
+  for(let element of array){
+    if(test(element)){
+      passed.push(element);
+    }
+  }
+  return passed;
+};
+console.log(filter(meals, price => price < 5));
+*/
+
 const raised = meals.map((price) => price * 1.15); //ei toimi
 console.log('Raised meals', raised);
 
+/*      //näyttää hinnat, mut ei korota niitä
+const map = (array, transform) =>{
+  let mapped = [];
+  for(let element of array){
+    mapped.push(transform(element));
+  }
+  return mapped;
+};
+let raised = meals.filter(s => s.price * 1.15);
+console.log('Price raised 15%', map(raised, s => s.price));
+*/
 const sum = meals.reduce((acc, price) => acc + price); //ei toimi
 console.log('Whole menu', sum);
+
+  //Antaa NaN
+const reduce = (array, combine, start) =>{
+  let current = start;
+  for(let element of array){
+    current = combine(current, element);
+  }
+  return current;
+};
+console.log(reduce([meals].price, (a, b) => a + b, 0));
 
