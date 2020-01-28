f70bb4afcfd3f23f83daf80d89c488980403af61
fannyj0 <fanny.jokinen@metropolia.fi>
2020-01-28 12:06:10 +0200

    w2t1 kokeilua

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/*Best algorithm would be binary algorithm where the computer first asks 50 and
+then if it is too high then 25 and so on halving it, or if it's too low it asks
+75 and adds more to it*/
+
 import {StartGame, checkGuess, resetGame} from './modules/guess-game';
 
 StartGame();
@@ -15,17 +19,20 @@ const testGamePlay = () => {
       gameOver = true;
       resetGame();
     }else if (correctGuess < 0){
+      //myGuess += 25; //too low, ask 75
       myGuess++;
     }else{
+      //myGuess -= 25; //too high, ask 25
       myGuess--;
     }
   }
   return guessCounter;
+  console.log("guesscounter ^");
 };
 //testGamePlay();
 
 let guessCounts = [];
-for(let i=0; i<15; i++){
+for(let i=0; i<10; i++){
   guessCounts.push(testGamePlay());
 }
 console.log('guess counts', guessCounts);
