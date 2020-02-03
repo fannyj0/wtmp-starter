3f9495226194b73dc1df5f3eaad26fffa74a70e7
fannyj0 <fanny.jokinen@metropolia.fi>
2020-02-03 17:48:26 +0200

    viikko3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('Hello console!');
+/* Game cheat code */
+/* document.addEventListener('keydown', event =>{
+  console.log('keydown', event.key, event.keyCode);
+  if(event.keyCode === 72 && event.keyCode === 69){
+    alert('Welcome brother');
+  }
+}); */
+
+/* x and y coordinates on mouse double clicks */
+document.getElementById("jaa").ondblclick = function() {fun();};
+
+const fun = () =>{
+  document.querySelector('body').addEventListener('click', (event) => {
+    let x = event.clientX;
+    let y = event.clientY;
+    const coords ='X: ' +x+ '\tY: ' +y;
+    document.getElementById("demo").innerHTML = coords;
+  });
+};
+
+/* element that reacts to touches but not clicks */
+document.addEventListener('keydown', event =>{
+  console.log('key pressed:', event.key, event.keyCode);
+});
+
+/* timer 15s browsing */
+setTimeout(() => alert("Hurry up!"), 10000);
+
+/* timer 15s idling */
