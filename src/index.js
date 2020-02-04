b8ace1ee3c1439c0eb8dea0d3b2fdd8a5bb6efb3
fannyj0 <fanny.jokinen@metropolia.fi>
2020-02-04 16:20:15 +0200

    viikko3 työstöä

--- src/index.js
+++ src/index.js
@@ -2,28 +2,39 @@
 /* document.addEventListener('keydown', event =>{
   console.log('keydown', event.key, event.keyCode);
   if(event.keyCode === 72 && event.keyCode === 69){
-    alert('Welcome brother');
+    alert('Secret unlocked');
   }
 }); */
 
 /* x and y coordinates on mouse double clicks */
-document.getElementById("jaa").ondblclick = function() {fun();};
+document.getElementById("test").ondblclick = () => {fun();};
 
 const fun = () =>{
-  document.querySelector('body').addEventListener('click', (event) => {
+  document.querySelector('body').addEventListener('dblclick', (event) => {
     let x = event.clientX;
     let y = event.clientY;
-    const coords ='X: ' +x+ '\tY: ' +y;
+    //console.log('X: ' +x+ '\tY: ' +y);
+
+    /* näkyy html */
+    const coords ='Mouse coordinates: X: ' +x+ '\tY: ' +y;
     document.getElementById("demo").innerHTML = coords;
   });
 };
 
 /* element that reacts to touches but not clicks */
-document.addEventListener('keydown', event =>{
-  console.log('key pressed:', event.key, event.keyCode);
-});
+  document.addEventListener('keydown', event =>{
+    console.log('key pressed:', event.key, event.keyCode);
+  });
+
 
 /* timer 15s browsing */
-setTimeout(() => alert("Hurry up!"), 10000);
+const timer = () =>{
+  if(fun === true){
+  console.log("hurry up!");
+  }
+};
+setTimeout(timer, 5000);
 
 /* timer 15s idling */
+/* setTimeout(() => alert("Hurry up!"), 5000);
+ */
